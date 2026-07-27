f180d9c64c170f18fd4a1a52b4c3a314101b9e29
Mihajasoa <mihajasoaalain85@gmail.com>
2026-07-27 16:28:48 +0300

    refactor search engine indexing to use term structure and track word positions

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -2,24 +2,14 @@ package engine
 
 import (
 	"github.com/miih/miih-search/internal/models"
-	"github.com/miih/miih-search/internal/tokenizer"
 )
 
 type SearchEngine struct {
-	index map[string][]string
+	index map[string]*models.Term
 }
 
 func NewSearchEngine() *SearchEngine {
 	return &SearchEngine{
-		index: make(map[string][]string),
+		index: make(map[string]*models.Term),
 	}
-}
-func (s *SearchEngine) Index(doc models.Document) {
-	words := tokenizer.Tokenize(doc.Content)
-	for _, word := range words {
-		s.index[word] = append(s.index[word], doc.ID)
-	}
-}
-func (s *SearchEngine) Search(word string) []string {
-	return s.index[word]
 }
