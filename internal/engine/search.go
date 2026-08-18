88a7c6ed35cdc985bf399bb4670a35a7433f133c
Mihajasoa <mihajasoaalain85@gmail.com>
2026-08-18 09:50:29 +0300

    chore: remove dead code and tidy module requirements
    internal/repository/ held three files containing nothing but a package
    declaration, models.Storage was a leftover struct duplicating the
    storage.Storage interface, and engine/search.go was entirely commented out.
    
    go mod tidy also promotes pgx, godotenv and x/text out of the indirect
    block, where they had been misfiled despite being direct dependencies.

--- internal/engine/search.go
+++
@@ -1,9 +0,0 @@
-package engine
-
-//func (s *SearchEngine) Search(word string) []models.Posting {
-//	term, ok := s.index[word]
-//	if !ok {
-//		return nil
-//	}
-//	return term.Postings
-//}
--- internal/models/storage.go
+++
@@ -1,5 +0,0 @@
-package models
-
-type Storage struct {
-	SaveDocument func(doc Document) error
-}
